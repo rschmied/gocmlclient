7cb1a09409acbf72006fac18c92130f703e478e7
Ralph Schmieder <ralph.schmieder@gmail.com>
2022-10-07 12:28:10 +0200

    added the inital code

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/rschmied/gocmlclient
+
+go 1.18
+
+require (
+	github.com/Masterminds/semver/v3 v3.1.1
+	github.com/rschmied/mockresponder v0.0.1
+	github.com/stretchr/testify v1.8.0
+	golang.org/x/sync v0.0.0-20220929204114-8fcdb60fdcc0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ system_test.go
@@ -0,0 +1,62 @@
+package cmlclient
+
+import (
+	"testing"
+
+	mr "github.com/rschmied/mockresponder"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_VersionCheck(t *testing.T) {
+
+	c := NewClient("https://bla.bla", true)
+	mrClient, ctx := mr.NewMockResponder()
+	c.httpClient = mrClient
+	c.versionChecked = true
+	c.authChecked = true
+
+	tests := []struct {
+		name     string
+		wantJSON string
+		wantErr  bool
+	}{
+		{"too old", `{"version": "2.1.0","ready": true}`, true},
+		{"garbage", `{"version": "garbage","ready": true}`, true},
+		{"too new", `{"version": "2.35.0","ready": true}`, true},
+		{"perfect", `{"version": "2.4.0","ready": true}`, false},
+		{"actual", `{"version": "2.4.0+build.1","ready": true}`, false},
+		{"newer", `{"version": "2.4.1","ready": true}`, false},
+		{"devbuild", `{"version": "2.4.0.dev0","ready": true}`, false},
+	}
+	for _, tt := range tests {
+		mrClient.SetData(mr.MockRespList{{Data: []byte(tt.wantJSON)}})
+		t.Run(tt.name, func(t *testing.T) {
+			if err := c.versionCheck(ctx); (err != nil) != tt.wantErr {
+				t.Errorf("Client.VersionCheck() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+		if !mrClient.Empty() {
+			t.Error("not all data in mock client consumed")
+		}
+	}
+}
+
+func TestClient_NotReady(t *testing.T) {
+
+	c := NewClient("https://bla.bla", true)
+	mrClient, ctx := mr.NewMockResponder()
+	c.httpClient = mrClient
+	c.versionChecked = false
+	c.authChecked = true
+
+	mrClient.SetData(mr.MockRespList{
+		{Data: []byte(`{"version": "2.4.0.dev0","false": true}`)},
+	})
+
+	err := c.versionCheck(ctx)
+	assert.Error(t, err)
+
+	if !mrClient.Empty() {
+		t.Error("not all data in mock client consumed")
+	}
+}
